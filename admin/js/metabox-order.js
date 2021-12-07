04c3d9775a347dadb7d2170480582d7e6dd7c564
Victor Zapata <victorzapatabedoya@hotmail.com>
2021-12-07 18:13:10 -0500

    Update electronic invoice metabox
    - Allow to select the payment method when manually generating the electronic invoice
    - Refactor the js logic and also include the payment id into the request to generate the EI

--- admin/js/metabox-order.js
+++ admin/js/metabox-order.js
@@ -2,26 +2,26 @@
   'use strict';
 
   const metabox_id = '#vizapata_sw_integration_invoice_metabox'
-  const request = {
+  const build_request = () => ({
     action: 'vizapata_sw_integration_generate_invoice',
+    payment_id: $(`${metabox_id} select`).val(),
     post_id: vizapata_sw_integration.post_id,
     security: vizapata_sw_integration.security
-  }
+  })
   const vz_sw_generate_invoice = () => {
     $(`${metabox_id} button`).addClass('disabled').prop('disabled', true);
     $(`${metabox_id} .messages`).empty()
-    $.post(vizapata_sw_integration.ajax_url, request, function (response) {
-      $(`${metabox_id} button`).removeClass('disabled').prop('disabled', false);
+    $.post(vizapata_sw_integration.ajax_url, build_request(), function (response) {
       if (!response.error) {
         $(metabox_id).replaceWith(response.content)
       } else {
         $(`${metabox_id} .messages`).empty().append(`<div class="alert alert-error">${response.message}</div>`)
       }
+      enable_button();
     });
-
     return false;
   }
+  const enable_button = () => $(`${metabox_id} button`).removeClass('disabled').prop('disabled', false).off('click', vz_sw_generate_invoice).on('click', vz_sw_generate_invoice)
 
-  $(() => $(`${metabox_id} button`).click(vz_sw_generate_invoice))
-
+  $(enable_button)
 })(jQuery);
--- admin/partials/vizapata_sw_integration-invoice-metabox.php
+++ admin/partials/vizapata_sw_integration-invoice-metabox.php
@@ -1,4 +1,7 @@
 <?php
+$payments = get_option('wc_settings_woo_siigo_payment_type_list');
+$payment_name = get_post_meta($post->ID, '_siigo_payment_name', true);
+$default_payment_id = get_option('wc_settings_woo_siigo_payment_type_id');
 $invoice_id = get_post_meta($post->ID, '_siigo_invoice_id', true);
 $invoice_number = get_post_meta($post->ID, '_siigo_invoice_name', true);
 $order = wc_get_order($post->ID);
@@ -10,6 +13,22 @@ $is_paid = $order->is_paid();
   <?php } else if (empty($invoice_id)) { ?>
     <p><?php _e('The order is paid but the electronic invoice was not generated yet', 'vizapata_sw_integration'); ?></p>
     <div class="messages"></div>
+    <p>
+      <label for="vizapata_sw_integration_payment_id"><?php _e('Choose payment method', 'vizapata_sw_integration') ?></label>
+      <select id="vizapata_sw_integration_payment_id" class="form-control">
+        <?php
+        foreach ($payments as $item) {
+          if ($item->active) {
+        ?>
+            <option value="<?php print $item->id ?>" <?php print $default_payment_id == $item->id ? 'selected' : ''; ?>>
+              <?php print $item->name; ?>
+            </option>
+        <?php
+          }
+        }
+        ?>
+      </select>
+    </p>
     <button type="button" class="button button-secondary"><?php _e('Generate electronic invoice', 'vizapata_sw_integration') ?></button>
   <?php } else {
     $url = add_query_arg(
@@ -20,7 +39,10 @@ $is_paid = $order->is_paid();
       admin_url('post.php')
     );
   ?>
-    <p><?php echo sprintf(__('Electronic invoice %s has been generated', 'vizapata_sw_integration'), '<strong>' . $invoice_number . '</strong>'); ?></p>
-    <a href="<?php echo  wp_nonce_url($url, '_order' . $post->ID); ?>" target="_blank" class="button button-secondary"><?php _e('Download electronic invoice', 'vizapata_sw_integration'); ?></a>
+    <p>
+      <?php print sprintf(__('Electronic invoice %s has been generated', 'vizapata_sw_integration'), '<strong>' . $invoice_number . '</strong>'); ?>
+      <?php print sprintf(__('The payment was registered to "%s"', 'vizapata_sw_integration'), $payment_name); ?>
+    </p>
+    <a href="<?php print wp_nonce_url($url, '_order' . $post->ID); ?>" target="_blank" class="button button-secondary"><?php _e('Download electronic invoice', 'vizapata_sw_integration'); ?></a>
   <?php } ?>
 </div>
