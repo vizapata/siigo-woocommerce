e42255cc49a8813f5e9a62cfe447b08095ddf9cd
Victor Zapata <victorzapatabedoya@hotmail.com>
2021-12-07 11:49:54 -0500

    Delete unused admin file

--- admin/partials/vizapata_sw_integration-admin-display.php
+++
@@ -1,7 +0,0 @@
-<form action='options.php' method='post'>
-  <?php
-  settings_fields('vizapata_sw_integration_options');
-  do_settings_sections('vizapata_sw_integration_options');
-  submit_button();
-  ?>
-</form>
